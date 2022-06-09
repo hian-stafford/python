51165b855eb4aa9bc5cf7516563edc8783f9602e
Hian Stafford <82848416+Hian-Stafford@users.noreply.github.com>
2022-06-09 10:41:57 -0300

    Create validador de cpf.py
    valida o cpf descrito "16899535009"

---
+++ gerador e validador de cpf/validador de cpf.py
@@ -0,0 +1,28 @@
+while True:
+    #cpf = '16899535009'
+    cpf = input('Digite um cpf: ')
+    novo_cpf = cpf [:-2] # elimina os 2 últimos dígitos do cpf
+    reverso = 10 # contador reverso
+    soma = 0
+
+
+    for index in range(19):
+        if index > 8:   # primeiro índice vai de 0 a 9,
+            index -= 9  # são os 9 primeiros digitos do cpf
+            soma += int(novo_cpf[index]) * reverso # valor total da multiplicação
+
+        reverso -= 1    #decrementa o contador reverso
+        if reverso < 2:
+            reverso = 11
+            d = 11 - (soma % 11)
+
+            if d > 9:   # se o digito for > que 9 o valor é 0
+                d = 0
+            soma = 0    #zera a soma para a nova contagem
+            novo_cpf += str(d)  #concatena o digito gerando o novo cpf
+    print(novo_cpf)
+    
+        if cpf == novo_cpf:    #verifica se o novo cpf é igual ao novo cpf
+        print('cpf válido')
+    else:
+        print('cpf inválido')
