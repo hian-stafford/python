5f803d51bbcb1003d68e5ec009f0afbf1970eb9b
Hian <hian.correa@gmail.com>
2022-06-13 23:42:21 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,22 @@
+# d1 = dict (chave1 = 'Valor da chave', chave2 = 'valor da outra chave')#cria a chave e o valor da chave
+# d1 ['nova_chave'] = 'Valor da nova chave' #adiciona uma nova chave no dicionário
+# print(d1) 
+
+
+# d1 = {'chave1':'Valor', 'chave1':'Valor', 'chave1':'Valor final da chave'} #por serem a mesma chave, ele considera apenas o valor final
+
+# print(d1)
+
+
+# d1 = {1: 'Valor1', 2: 'Valor2', 3: 'valor real da nova chave'} #por serem chaves diferentes, os valores são diferentes
+# print(d1[1])
+
+
+
+d1 = {
+'str' : 'valor',
+123: 'Outro valor',
+(1,2,3,4) : 'Tupla'
+}
+
+print(d1[(1,2,3,4)])
